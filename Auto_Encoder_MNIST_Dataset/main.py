2375a1ad84c3c1c19f15f0d888e068df77cf2d33
Imman <immanuelsavio@gmail.com>
2019-05-22 17:05:09 +0530

    added code for data download and preprocessing

--- Auto_Encoder_MNIST_Dataset/main.py
+++ Auto_Encoder_MNIST_Dataset/main.py
@@ -1,1 +1,35 @@
-This is the main file
+#imports and modules
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+import keras
+import matplotlib.pyplot as plt
+import numpy as np
+import gzip
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D
+from keras.models import Model
+from keras.datasets import fashion_mnist
+from keras.optimizers import RMSprop
+
+#def data_reading():
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+print(x_train.shape, x_test.shape)
+#data_reading()
+
+label_dict = {
+ 0: 'A',
+ 1: 'B',
+ 2: 'C',
+ 3: 'D',
+ 4: 'E',
+ 5: 'F',
+ 6: 'G',
+ 7: 'H',
+ 8: 'I',
+ 9: 'J',
+}
+
+x_train = x_train.reshape(-1,28,28,1)
+x_test = x_test.reshape(-1,28,28,1)
+print(x_train.shape)
+print(x_test.shape)
