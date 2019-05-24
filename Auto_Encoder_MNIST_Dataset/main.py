b7fc293448567c0688e298a74ce25850910d24d4
Imman <immanuelsavio@gmail.com>
2019-05-24 11:09:14 +0530

    added the classifier and the validation

--- Auto_Encoder_MNIST_Dataset/main.py
+++ Auto_Encoder_MNIST_Dataset/main.py
@@ -9,6 +9,7 @@ import gzip
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D
 from keras.models import Model
 from keras.datasets import fashion_mnist
+from keras.layers.normalization import BatchNormalization
 from keras.optimizers import RMSprop
 from sklearn.model_selection import train_test_split
 
@@ -44,7 +45,7 @@ x_test = x_test/np.max(x_test)
 
 print(np.max(x_test), np.max(x_train))
 
-train_x, valid_x, train_ground, test_ground = train_test_split(x_train, x_train, test_size = 0.2, random_state = 12)
+train_x, valid_x, train_ground, valid_ground = train_test_split(x_train, x_train, test_size = 0.2, random_state = 12)
 print(train_x.shape, valid_x.shape)
 
 batch_size = 64
@@ -94,3 +95,96 @@ def decoder(conv4):
     up2 = UpSampling2D((2,2))(conv7) # 28 x 28 x 32
     decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(up2) # 28 x 28 x 1
     return decoded
+
+autoencoder = Model(input_img, decoder(encoder(input_img)))
+autoencoder.compile(loss='mean_squared_error', optimizer = RMSprop())
+autoencoder.summary()
+autoencoder_train = autoencoder.fit(train_x, 
+                                    train_ground, 
+                                    batch_size=batch_size,
+                                    epochs=epochs,
+                                    verbose=1,
+                                    validation_data=(valid_x, valid_ground))
+autoencoder.save_weights('autoencoder.h5')
+
+#using the auto encoder
+train_y_one_hot = to_categorical(train_labels)
+test_Y_one_hot = to_categorical(test_labels)
+print('Original label:', train_labels[0])
+print('After conversion to one-hot:', train_y_one_hot[0])
+
+train_x,valid_x,train_label,valid_label = train_test_split(train_data,train_y_one_hot,test_size=0.2,random_state=13)
+train_x.shape,valid_x.shape,train_label.shape,valid_label.shape
+
+def encoder(input_img):
+    #encoder
+    #input = 28 x 28 x 1 (wide and thin)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img) #28 x 28 x 32
+    conv1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    conv1 = BatchNormalization()(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) #14 x 14 x 32
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1) #14 x 14 x 64
+    conv2 = BatchNormalization()(conv2)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) #7 x 7 x 64
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2) #7 x 7 x 128 (small and thick)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv3) #7 x 7 x 256 (small and thick)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    conv4 = BatchNormalization()(conv4)
+    return conv4
+
+def fc(enco):
+    flat = Flatten()(enco)
+    den = Dense(128, activation='relu')(flat)
+    out = Dense(num_classes, activation='softmax')(den)
+    return out
+
+encode = encoder(input_img)
+full_model = Model(input_img,fc(encode))
+
+for l1,l2 in zip(full_model.layers[:19],autoencoder.layers[0:19]):
+    l1.set_weights(l2.get_weights())
+
+autoencoder.get_weights()[0][1]
+
+full_model.get_weights()[0][1]
+for layer in full_model.layers[0:19]:
+    layer.trainable = False
+
+full_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+full_model.summary()
+classify_train = full_model.fit(train_x, train_label, batch_size=64,epochs=100,verbose=1,validation_data=(valid_x, valid_label))
+full_model.save_weights('autoencoder_classification.h5')
+full_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+classify_train = full_model.fit(train_x, train_label, batch_size=64,epochs=100,verbose=1,validation_data=(valid_x, valid_label))
+full_model.save_weights('classification_complete.h5')
+accuracy = classify_train.history['acc']
+val_accuracy = classify_train.history['val_acc']
+loss = classify_train.history['loss']
+val_loss = classify_train.history['val_loss']
+epochs = range(len(accuracy))
+
+test_eval = full_model.evaluate(x_test, test_Y_one_hot, verbose=0)
+print('Test loss:', test_eval[0])
+print('Test accuracy:', test_eval[1])
+
+predicted_classes = full_model.predict(x_test)
+
+
+predicted_classes = np.argmax(np.round(predicted_classes),axis=1)
+predicted_classes.shape, x_test.shape
+correct = np.where(predicted_classes==y_test)[0]
+print ("Found %d correct labels" % len(correct))
+
+incorrect = np.where(predicted_classes!=test_labels)[0]
+print ("Found %d incorrect labels" % len(incorrect))
+
+from sklearn.metrics import classification_report
+target_names = ["Class {}".format(i) for i in range(num_classes)]
+print(classification_report(test_labels, predicted_classes, target_names=target_names))
