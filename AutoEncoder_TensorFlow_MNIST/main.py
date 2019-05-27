35e94329cc12d7911534ea05789e6bfecb27bae0
Imman <immanuelsavio@gmail.com>
2019-05-27 15:38:53 +0530

    Added new codes

--- AutoEncoder_TensorFlow_MNIST/main.py
+++ AutoEncoder_TensorFlow_MNIST/main.py
@@ -1,1 +1,8 @@
-This is the main file
+#!/usr/bin/python
+import numpy as np 
+import matplotlib.pyplot as plt 
+import tensorflow as tf
+import tensorflow.examples.tutorials.mnist.input_data as input_data
+
+df = input_data.read_data_sets("MNIST_data", one_hot=True)
+print(df)
