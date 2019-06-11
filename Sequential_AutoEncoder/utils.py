54b9d8d353e77cccdc45e26f4b1df6bd988dff88
Imman <immanuelsavio@gmail.com>
2019-06-11 17:43:40 +0530

    added new code

--- Sequential_AutoEncoder/utils.py
+++ Sequential_AutoEncoder/utils.py
@@ -4,6 +4,22 @@ import numpy as np
 import math
 import sys
 
+
+def batch_iterator(X, y=None, batch_size=64):
+    """ Simple batch generator """
+    n_samples = X.shape[0]
+    for i in np.arange(0, n_samples, batch_size):
+        begin, end = i, min(i+batch_size, n_samples)
+        if y is not None:
+            yield X[begin:end], y[begin:end]
+        else:
+            yield X[begin:end]
+
+bar_widgets = [
+    'Training: ', progressbar.Percentage(), ' ', progressbar.Bar(marker="-", left="[", right="]"),
+    ' ', progressbar.ETA()
+]
+
 def make_diagonal(x):
     m = np.zeros((len(x),len(x)))
     for i in range (len(m[0])):
