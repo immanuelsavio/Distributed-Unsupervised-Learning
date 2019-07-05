f3b9e7b6e071eeca594ac0fbb2472b6502ee1a87
Imman <immanuelsavio@gmail.com>
2019-07-06 00:41:14 +0530

    aaddded main

--- Sequential_Autoencoder_Keras/main.py
+++ Sequential_Autoencoder_Keras/main.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import gzip
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 from keras.models import Model
 from keras.optimizers import RMSprop
 from keras.layers import Input,Dense,Flatten,Dropout,merge,Reshape,Conv2D,MaxPooling2D,UpSampling2D,Conv2DTranspose
@@ -13,6 +14,73 @@ from keras.optimizers import Adadelta, RMSprop,SGD,Adam
 from keras import regularizers
 from keras import backend as K
 from keras.utils import to_categorical
+
+
 from keras.datasets import mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(-1, 28,28, 1)
+x_test = x_test.reshape(-1, 28,28, 1)
+
+x_train = x_train/np.max(x_train)
+x_test = x_test/np.max(x_test)
+
+print(np.max(x_train))
+train_X,valid_X,train_ground,valid_ground = train_test_split(x_train,
+                                                             x_train,
+                                                             test_size=0.2,
+                                                             random_state=13)
+
+batch_size = 64
+epochs = 200
+inChannel = 1
+x, y = 28, 28
+input_img = Input(shape = (x, y, inChannel))
+num_classes = 10
+
+def encoder(input_img):
+    #encoder
+    #input = 28 x 28 x 1 (wide and thin)
+    conv1 = Conv2D(256, (3, 3), activation='relu', padding='same')(input_img) #28 x 28 x 32
+    conv1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv1)
+    conv1 = BatchNormalization()(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) #14 x 14 x 32
+    conv2 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool1) #14 x 14 x 64
+    conv2 = BatchNormalization()(conv2)
+    conv2 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) #7 x 7 x 64
+    conv3 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool2) #7 x 7 x 128 (small and thick)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conv4 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv3) #7 x 7 x 256 (small and thick)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv4)
+    conv4 = BatchNormalization()(conv4)
+    return conv4
+
+def decoder(conv4):    
+    #decoder
+    conv5 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv4) #7 x 7 x 128
+    conv5 = BatchNormalization()(conv5)
+    conv5 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv5)
+    conv5 = BatchNormalization()(conv5)
+    conv6 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv5) #7 x 7 x 64
+    conv6 = BatchNormalization()(conv6)
+    conv6 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv6)
+    conv6 = BatchNormalization()(conv6)
+    up1 = UpSampling2D((2,2))(conv6) #14 x 14 x 64
+    conv7 = Conv2D(256, (3, 3), activation='relu', padding='same')(up1) # 14 x 14 x 32
+    conv7 = BatchNormalization()(conv7)
+    conv7 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv7)
+    conv7 = BatchNormalization()(conv7)
+    up2 = UpSampling2D((2,2))(conv7) # 28 x 28 x 32
+    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(up2) # 28 x 28 x 1
+    return decoded
+
+autoencoder = Model(input_img, decoder(encoder(input_img)))
+autoencoder.compile(loss='mean_squared_error', optimizer = RMSprop())
+
