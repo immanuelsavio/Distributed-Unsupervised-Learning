264fa007258571c10aba7995ae5f34543699a854
Imman <immanuelsavio@gmail.com>
2019-07-07 21:56:02 +0530

    averaging autoencoder is complete and ready for deployment

--- Sequential_Autoencoder_Keras/Averaging_Sequential_Autoencoder/main.py
+++ Sequential_Autoencoder_Keras/Averaging_Sequential_Autoencoder/main.py
@@ -166,3 +166,91 @@ plt.plot(epochs, val_loss, 'b', label='Validation loss')
 plt.title('Training and validation loss')
 plt.legend()
 plt.show()
+
+autoencoder_final.save_weights('autoencoder.h5')
+
+x = autoencoder_final.get_weights()
+print(x)
+
+(_ , y_train), (_, y_test) = mnist.load_data()
+train_Y_one_hot = to_categorical(y_train)
+test_Y_one_hot = to_categorical(y_test)
+print('Original label:', y_train[0])
+print('After conversion to one-hot:', train_Y_one_hot[0])
+
+train_X,valid_X,y_train,y_valid = train_test_split(X_train,train_Y_one_hot,test_size=0.2,random_state=13)
+train_X.shape,valid_X.shape,y_train.shape,y_valid.shape
+
+def fc(enco):
+    flat = enco
+    den = Dense(128, activation='relu')(flat)
+    out = Dense(num_classes, activation='softmax')(den)
+    return out
+
+full_model = Model(input_img,fc(Encoder_part(input_img)))
+
+for l1,l2 in zip(full_model.layers[:6],autoencoder_final.layers[0:6]):
+    l1.set_weights(l2.get_weights())
+
+autoencoder_final.get_weights()[0][1]
+
+full_model.get_weights()[0][1]
+
+for layer in full_model.layers[0:6]:
+    layer.trainable = False
+
+full_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+
+full_model.summary()
+
+for layer in full_model.layers[0:19]:
+    layer.trainable = False
+
+classify_train = full_model.fit(train_X, y_train, batch_size=64,epochs=100,verbose=1,validation_data=(valid_X, y_valid))
+
+full_model.save_weights('autoencoder_classification.h5')
+
+for layer in full_model.layers[0:19]:
+    layer.trainable = True
+
+full_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+
+classify_train = full_model.fit(train_X, y_train, batch_size=64,epochs=100,verbose=1,validation_data=(valid_X, y_valid))
+
+full_model.save_weights('classification_complete.h5')
+
+accuracy = classify_train.history['acc']
+
+val_accuracy = classify_train.history['val_acc']
+
+loss = classify_train.history['loss']
+
+val_loss = classify_train.history['val_loss']
+
+epochs = range(len(accuracy))
+
+plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+test_eval = full_model.evaluate(X_test, test_Y_one_hot, verbose=0)
+
+print('Test loss:', test_eval[0])
+print('Test accuracy:', test_eval[1])
+
+predicted_classes = full_model.predict(X_test)
+predicted_classes = np.argmax(np.round(predicted_classes),axis=1)
+predicted_classes.shape, y_test.shape
+
+correct = np.where(predicted_classes==y_test)[0]
+print("Found %d correct labels" % len(correct))
+
+incorrect = np.where(predicted_classes!=y_test)[0]
+print ("Found %d incorrect labels" % len(incorrect))
