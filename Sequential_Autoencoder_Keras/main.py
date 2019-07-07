26241a329532f536ad9c9d01ebc7fb73133e0a17
Imman <immanuelsavio@gmail.com>
2019-07-07 13:49:42 +0530

    finished and ready for deployment

--- Sequential_Autoencoder_Keras/main.py
+++ Sequential_Autoencoder_Keras/main.py
@@ -29,7 +29,7 @@ print(X_train.shape)
 print(X_test.shape)
 
 batch_size = 64
-epochs = 1
+epochs = 100
 inChannel = 1
 x, y = 28, 28
 input_img = Input(shape = (x, y, inChannel))
@@ -61,7 +61,7 @@ def Encoder_part(input_img):
     return encoded
 
 def Decoder_part(encoded):
-
+        
     decoded = Dense(units=64, activation='relu')(encoded)
     decoded = Dense(units=128, activation='relu')(decoded)
     decoded = Dense(units=256, activation='relu')(decoded)
@@ -70,19 +70,17 @@ def Decoder_part(encoded):
     
     return decoded
 
-
 autoencoder=Model(input_img, Decoder_part(Encoder_part(input_img)))
 encoder = Model(input_img, Encoder_part(input_img))
 
 autoencoder.summary()
-
-
 autoencoder.compile(loss='mean_squared_error', optimizer = SGD())
 autoencoder_train = autoencoder.fit(train_X, train_ground, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_X, valid_ground))
 
 loss = autoencoder_train.history['loss']
 val_loss = autoencoder_train.history['val_loss']
-epochs = range(1)
+epochs = range(100)
+
 plt.figure()
 plt.plot(epochs, loss, 'bo', label='Training loss')
 plt.plot(epochs, val_loss, 'b', label='Validation loss')
@@ -95,17 +93,14 @@ autoencoder.save_weights('autoencoder.h5')
 x = autoencoder.get_weights()
 print(x)
 
+(_ , y_train), (_, y_test) = mnist.load_data()
 train_Y_one_hot = to_categorical(y_train)
 test_Y_one_hot = to_categorical(y_test)
 print('Original label:', y_train[0])
 print('After conversion to one-hot:', train_Y_one_hot[0])
 
-train_X,valid_X,train_label,valid_label = train_test_split(X_train,train_Y_one_hot,test_size=0.2,random_state=13)
-
-train_Y_one_hot = to_categorical(y_train)
-test_Y_one_hot = to_categorical(y_test)
-
-train_X,valid_X,train_label,valid_label = train_test_split(X_train,train_Y_one_hot,test_size=0.2,random_state=13)
+train_X,valid_X,y_train,y_valid = train_test_split(X_train,train_Y_one_hot,test_size=0.2,random_state=13)
+train_X.shape,valid_X.shape,y_train.shape,y_valid.shape
 
 def fc(enco):
     flat = enco
@@ -113,14 +108,60 @@ def fc(enco):
     out = Dense(num_classes, activation='softmax')(den)
     return out
 
-encode = encoder(input_img)
-full_model = Model(input_img,fc(encode))
+full_model = Model(input_img,fc(Encoder_part(input_img)))
 
-#5 hidden layers + 1 input layer
 for l1,l2 in zip(full_model.layers[:6],autoencoder.layers[0:6]):
     l1.set_weights(l2.get_weights())
 
 autoencoder.get_weights()[0][1]
 
 full_model.get_weights()[0][1]
-#Both the layer weights should match
+
+for layer in full_model.layers[0:6]:
+    layer.trainable = False
+
+full_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+
+full_model.summary()
+
+for layer in full_model.layers[0:19]:
+    layer.trainable = False
+
+classify_train = full_model.fit(train_X, y_train, batch_size=64,epochs=100,verbose=1,validation_data=(valid_X, y_valid))
+
+full_model.save_weights('autoencoder_classification.h5')
+
+for layer in full_model.layers[0:19]:
+    layer.trainable = True
+
+full_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+
+classify_train = full_model.fit(train_X, y_train, batch_size=64,epochs=100,verbose=1,validation_data=(valid_X, y_valid))
+
+full_model.save_weights('classification_complete.h5')
+
+accuracy = classify_train.history['acc']
+
+val_accuracy = classify_train.history['val_acc']
+
+loss = classify_train.history['loss']
+
+val_loss = classify_train.history['val_loss']
+
+epochs = range(len(accuracy))
+
+plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+test_eval = full_model.evaluate(X_test, test_Y_one_hot, verbose=0)
+
+print('Test loss:', test_eval[0])
+print('Test accuracy:', test_eval[1])
